e19da128ba55e745b1429dd21a20efa9200d1c52
nikkkushkaaafx <42305758+nikkkushkaaafx@users.noreply.github.com>
2018-08-12 02:29:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const config = require('./config.json');
+const config = require('./rainbow.json');
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
