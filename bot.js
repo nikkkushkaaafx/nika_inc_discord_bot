b8c0977cfd3497e0fee5eb12466a36337b6881ab
nikkkushkaaafx <42305758+nikkkushkaaafx@users.noreply.github.com>
2018-08-12 01:40:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,14 +12,3 @@ client.on(‘message’, message => {
 });
 
 client.login(‘NDc3ODg4NDI3NDY0MDY1MDQ0.DlDrQA.sWiD1iSn-ZabcaJ5Mpe_arDk0aM’);
-
-    static void UpdatePresence()
-    {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "by nikkkushkaaafx";
-        discordPresence.details = "Special for Nika Inc";
-        discordPresence.startTimestamp = 0;
-        discordPresence.endTimestamp = 0;
-        Discord_UpdatePresence(&discordPresence);
-    }
