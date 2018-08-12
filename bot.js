54fbd83b678ad064dfb54ad7a4e6e39229a25ead
nikkkushkaaafx <42305758+nikkkushkaaafx@users.noreply.github.com>
2018-08-12 15:12:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ bot.on("message", async message => {
   }
   if (cmd === `${prefix}правила`){
     message.channel.send("Привет! Я автоматизированный бот помощник! Сейчас я расскажу тебе правила данного DISCORD сервера.");
-    message.channel.sendblock("qweqwe");
   }
 });
 
